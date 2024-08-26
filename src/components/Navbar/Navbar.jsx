18c01632cda2f4c2843ad94b53b1d2368a16c243
Harshit Negi <141933477+Harshitnegi5@users.noreply.github.com>
2024-08-26 16:38:29 +0530

    add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import logo from "../../assets/logo.png"
+import search_icon from "../../assets/search_icon.svg"
+import bell_icon from "../../assets/bell_icon.svg"
+import caret_icon from "../../assets/caret_icon.svg"
+import profile_img from "../../assets/profile_img.png"
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      navbar
+      <div className="nav-left">
+        <img src={logo} alt="" />
+        <ul>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>New & Popular</li>
+            <li>My List</li>
+            <li>Browse by Languages</li>
+        </ul>
+      </div>
+      <div className="nav-right">
+      <img src={search_icon} alt="" />
+      <p>Children</p>
+      <img src={bell_icon} alt="" />
+      <div className="profile">
+        <img src={profile_img} alt=""  />
+        <img src={caret_icon} alt="" />
+      </div>
+      </div>
     </div>
   )
 }
