26b53292818bd8ad79d3830611f5935e2055d85d
Harshit Negi <141933477+Harshitnegi5@users.noreply.github.com>
2024-08-26 17:05:32 +0530

    initial changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './pages/Home/Home'
+import Home from './components/Home/Home'
 
 const App = () => {
   return (
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Home.css"
-import Navbar from '../../components/Navbar/Navbar'
+import Navbar from '../Navbar/Navbar'
 
 const Home = () => {
   return (
