c16b251df034b4fa793c56477533609bec2496f4
Harshit Negi <141933477+Harshitnegi5@users.noreply.github.com>
2024-08-27 14:18:00 +0530

    add Sign IN and UP feature

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 import logo from "../../assets/logo.png";
 
 const Login = () => {
+  const [signState, SetsignState] = useState("Sign In");
+
   const btnHandler = (e) => {
     e.preventDefault();
   };
@@ -10,12 +12,14 @@ const Login = () => {
     <div className="login">
       <img src={logo} className="login-logo" alt="" />
       <div className="login-form">
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form>
-          <input type="text" placeholder="Your name" id="" />
+          {signState == "Sign Up" && (
+            <input type="text" placeholder="Your name" id="" />
+          )}
           <input type="email" placeholder="Email" id="" />
           <input type="password" placeholder="Password" id="" />
-          <button onClick={btnHandler}>Sign Up</button>
+          <button onClick={btnHandler}>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -24,6 +28,31 @@ const Login = () => {
             <p>need help?</p>
           </div>
         </form>
+        <div className="form-switch">
+          {signState == "Sign In" ? (
+            <p>
+              New to netflix?{" "}
+              <span
+                onClick={() => {
+                  SetsignState("Sign Up");
+                }}
+              >
+                Sign Up Now
+              </span>
+            </p>
+          ) : (
+            <p>
+              Already have account?{" "}
+              <span
+                onClick={() => {
+                  SetsignState("Sign In");
+                }}
+              >
+                Sign In Now
+              </span>
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
